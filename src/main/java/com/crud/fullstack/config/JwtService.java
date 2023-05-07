faf0ccec749629f9a9aeea73e9ef0c5aafa58b5d
krszar <krzysztofszarek2000@gmail.com>
2023-05-07 20:28:48 +0200

    backend jwt basics

---
+++ src/main/java/com/crud/fullstack/config/JwtService.java
@@ -0,0 +1,37 @@
+package com.crud.fullstack.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String KEY = "38782F413F4428472B4B6250655368566D597133743677397A24422645294840";
+
+    public String extractNick(String jwt){
+        return extractClaims(jwt).getSubject();
+    }
+
+
+
+    private Key getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Claims extractClaims(String jwt){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
+    }
+
+}
