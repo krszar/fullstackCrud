a431a468d19fcdc974e3d2fe799b77cddafef403
krszar <krzysztofszarek2000@gmail.com>
2023-05-12 21:58:09 +0200

    adding securityFilterChain

---
+++ fullstack/src/main/java/com/crud/fullstack/config/HttpConfig.java
@@ -0,0 +1,40 @@
+package com.crud.fullstack.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class HttpConfig {
+
+  private final AuthenticationProvider authenticationProvider;
+  private final SecurityConfig securityConfig;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+    return httpSecurity
+      .csrf().disable()
+      .authorizeHttpRequests()
+        .requestMatchers("").permitAll()
+        .anyRequest().authenticated()
+      .and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and()
+      .authenticationProvider(authenticationProvider)
+      .addFilterBefore(securityConfig, UsernamePasswordAuthenticationFilter.class)
+      .build();
+
+
+  }
+
+}
--- fullstack/src/main/java/com/crud/fullstack/config/UserDetailsConfig.java
+++ fullstack/src/main/java/com/crud/fullstack/config/UserDetailsConfig.java
@@ -5,6 +5,7 @@ import com.crud.fullstack.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,4 +22,7 @@ public class UserDetailsConfig {
     return username -> userRepository.findByEmail(username);
   }
 
+  @Bean public AuthenticationProvider authenticationProvider(){
+
+  }
 }
